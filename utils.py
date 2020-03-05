b41084c35bde978055f537051a9ef615be26b8ca
yassine.mimouni@etu.umontpellier.fr <mimouni.yassine84@gmail.com>
2020-03-05 16:59:11 +0100

    ajout de la fonction iteration_jeu au module utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,39 @@
 #!/net/apps/anaconda3/bin/python
 
-
-## Définition de la fonction qui renvoie une matrice de même taille que Z contenant le nombre de voisins de chaque élément
-
-
 def calcul_nb_voisins(Z):
-    forme= len(Z), len(Z[0])
-    N = [[0, ] * (forme[0]) for i in range(forme[1])]  # Créé une matrice N de 0 de taille forme[0] * forme[1]
-    for x in range(1, forme[0] -1):                    # Calcul du nombre de voisins
-        for y in range(1, forme[1] - 1):               # Le " + 0 " est nécessaire si tous les voisins sont morts
+    
+    '''Fonction calcul_nb_voisin qui renvoie une matrice de même taille que Z contenant le nombre de voisins de chaque élément de Z '''
+    
+    
+    forme = len(Z), len(Z[0])
+    N = [[0, ] * (forme[0]) for i in range(forme[1])]
+    for x in range(1, forme[0] -1):
+        for y in range(1, forme[1] - 1):
             N[x][y] = Z[x-1][y-1] + Z[x][y-1] + Z[x+1][y-1] + Z[x-1][y] + 0 + Z[x+1][y] + Z[x-1][y+1] + Z[x][y+1] + Z[x+1][y+1]
     return N                     
+
+
 
 import numpy as np
 
 
-Z =[[0,0,0,0,0,0],[0,0,0,1,0,0],[0,1,0,1,0,0],[0,0,1,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
-Z 
+def iteration_jeu(Z):
+    
+    '''Fonction itérative qui renvoie la matrice Z après un tour du jeu de la vie
+       Fait appel à la fonction calcul_nb_voisin'''    
+    
+    
+    forme = len(Z), len(Z[0])
+    N = calcul_nb_voisins(Z)
+    for x in range(1, forme[0] -1):
+        for y in range(1, forme[1] - 1):
+            if Z[x][y] == 1 and (N[x][y]<2 or N[x][y]>3):
+                Z[x][y] = 0
+            elif Z[x][y] == 0 and N[x][y]==3:
+                Z[x][y] = 1
+               
+    return Z
 
 
-N = calcul_nb_voisins(Z)
-N
+import matplotlib as plt    
+    
